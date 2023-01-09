a7f84e61b26fab1947aaf71ca2a4e565a3fdba16
notYoosung <98567880+notYoosung@users.noreply.github.com>
2023-01-09 12:43:15 -0600

    moved functions into `setup`

--- sketch.js
+++ sketch.js
@@ -1,54 +1,53 @@
-function isThing(maybeisthing) {
-	return (maybeisthing === undefined || maybeisthing === NaN || maybeisthing === null)
-}
-
-function ParentClass() { }
-
-/**
- * @param {Object} classConfig
- * @returns {Object} C
- */
-function newClass(classConfig) {
-	/**
-	 * @constructor
-	 * @param {Object} config 
-	 * 
-	 */
-	var C = function (config) {
-		let this_ = this;
-		Object.assign(this, config);
-
-		// Assign default values to undefined properties
-		if (isThing(classConfig.defaults)) {
-			classConfig.defaults.forEach((elem, indx) => {
-				if (!isThing(this_[elem])) {
-					this_[elem] = classConfig.defaults[elem];
-				}
-			});
-		}
-
-		// Print an error if a non-optional property is undefined
-		if (isThing(classConfig.essentials)) {
-			classConfig.essentials.forEach((elem, indx) => {
-				if (!isThing(this_[elem])) {
-					console.log(`Err: Missing ${elem}.`)
-				}
-			});
-		}
-	};
-
-	return C;
-}
-
-
+var isThing, newClass, ParentClass;
 
 function setup() {
 	createCanvas(400, 400);
+
+	isThing = function (maybeisthing) {
+		return maybeisthing === undefined || maybeisthing === NaN || maybeisthing === null;
+	};
+
+	ParentClass = function () {};
+
+	/**
+	 * @param {Object} classConfig
+	 * @returns {Object} C
+	 */
+	newClass = function (classConfig) {
+		/**
+		 * @constructor
+		 * @param {Object} config
+		 *
+		 */
+		var C = function (config) {
+			let this_ = this;
+			Object.assign(this, config);
+
+			// Assign default values to undefined properties
+			if (isThing(classConfig.defaults)) {
+				classConfig.defaults.forEach((elem, indx) => {
+					if (!isThing(this_[elem])) {
+						this_[elem] = classConfig.defaults[elem];
+					}
+				});
+			}
+
+			// Print an error if a non-optional property is undefined
+			if (isThing(classConfig.essentials)) {
+				classConfig.essentials.forEach((elem, indx) => {
+					if (!isThing(this_[elem])) {
+						console.log(`Err: Missing ${elem}.`);
+					}
+				});
+			}
+		};
+
+		return C;
+	};
+
+	
 }
 
 function draw() {
 	background(220);
-
-
-
 }
